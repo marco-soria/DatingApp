9a3fe8d7a35b157b9077562a3750fc69e95392a5
marco-soria <marco.soriaa7@gmail.com>
2025-07-29 20:27:55 -0500

    added get and cors

--- client/src/app/app.ts
+++ client/src/app/app.ts
@@ -1,12 +1,28 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, inject } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrl: './app.css',
 })
 export class App {
+  private http = inject(HttpClient);
   protected title = 'client';
+
+  ngOnInit() {
+    this.http.get('https://localhost:5001/api/members').subscribe({
+      next: (response) => {
+        console.log(response);
+      },
+      error: (error) => {
+        console.error('Error fetching members:', error);
+      },
+      complete: () => {
+        console.log('Request completed');
+      },
+    });
+  }
 }
