d965b86dc742e4b163d9a45bae6e6c633e46a1f5
marco-soria <marco.soriaa7@gmail.com>
2025-07-30 22:56:42 -0500

    added middleware and extension method

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -3,20 +3,22 @@ using System.Text;
 using API.Data;
 using API.DTOs;
 using API.Entities;
+using API.Extensions;
+using API.Interfaces;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
 namespace API.Controllers
 {
 
-    public class AccountController(AppDbContext context) : BaseAPIController
+    public class AccountController(AppDbContext context, ITokenService tokenService) : BaseAPIController
     {
-        [HttpPost("register")] // api/account/register
-        public async Task<ActionResult<AppUser>> Register(RegisterDto registerDto)
+         [HttpPost("register")] // api/account/register
+        public async Task<ActionResult<UserDto>> Register(RegisterDto registerDto)
         {
-            if (await EmailExists(registerDto.Email)) return BadRequest("Email is already in use");
-            
-            var hmac = new HMACSHA512();
+            if (await EmailExists(registerDto.Email)) return BadRequest("Email taken");
+
+            using var hmac = new HMACSHA512();
 
             var user = new AppUser
             {
@@ -29,7 +31,26 @@ namespace API.Controllers
             context.Users.Add(user);
             await context.SaveChangesAsync();
 
-            return user;
+            return user.ToDto(tokenService);
+        }
+
+        [HttpPost("login")]
+        public async Task<ActionResult<UserDto>> Login(LoginDto loginDto)
+        {
+            var user = await context.Users.SingleOrDefaultAsync(x => x.Email == loginDto.Email);
+
+            if (user == null) return Unauthorized("Invalid email address");
+
+            using var hmac = new HMACSHA512(user.PasswordSalt);
+
+            var computedHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(loginDto.Password));
+
+            for (var i = 0; i < computedHash.Length; i++)
+            {
+                if (computedHash[i] != user.PasswordHash[i]) return Unauthorized("Invalid password");
+            }
+
+            return user.ToDto(tokenService);
         }
         
         private async Task<bool> EmailExists(string email)
