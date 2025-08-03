fa76801bf19b114cc8fef6fef9093bee4a989042
marco-soria <marco.soriaa7@gmail.com>
2025-08-03 16:00:42 -0500

    updating resources

--- client/src/app/app.config.ts
+++ client/src/app/app.config.ts
@@ -11,6 +11,7 @@ import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { lastValueFrom } from 'rxjs';
 import { errorInterceptor } from '../core/interceptors/error-interceptor';
 import { jwtInterceptor } from '../core/interceptors/jwt-interceptor';
+import { loadingInterceptor } from '../core/interceptors/loading-interceptor';
 import { InitService } from '../core/services/init-service';
 import { routes } from './app.routes';
 
@@ -19,7 +20,9 @@ export const appConfig: ApplicationConfig = {
     provideBrowserGlobalErrorListeners(),
     provideZonelessChangeDetection(),
     provideRouter(routes, withViewTransitions()),
-    provideHttpClient(withInterceptors([errorInterceptor, jwtInterceptor])),
+    provideHttpClient(
+      withInterceptors([errorInterceptor, jwtInterceptor, loadingInterceptor])
+    ),
     provideAppInitializer(async () => {
       const initService = inject(InitService);
 
