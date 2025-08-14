004d1a1edd6b5b7fe6a71cb60358d43d07e4e7ac
marco-soria <marco.soriaa7@gmail.com>
2025-08-14 08:44:09 -0500

    added messages functionality

---
+++ client/src/core/services/message-service.ts
@@ -0,0 +1,42 @@
+import { HttpClient, HttpParams } from '@angular/common/http';
+import { inject, Injectable } from '@angular/core';
+import { environment } from '../../environments/environment';
+import { Message } from '../../types/message';
+import { PaginatedResult } from '../../types/pagination';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class MessageService {
+  private baseUrl = environment.apiUrl;
+  private http = inject(HttpClient);
+
+  getMessages(container: string, pageNumber: number, pageSize: number) {
+    let params = new HttpParams();
+
+    params = params.append('pageNumber', pageNumber);
+    params = params.append('pageSize', pageSize);
+    params = params.append('container', container);
+
+    return this.http.get<PaginatedResult<Message>>(this.baseUrl + 'messages', {
+      params,
+    });
+  }
+
+  getMessageThread(memberId: string) {
+    return this.http.get<Message[]>(
+      this.baseUrl + 'messages/thread/' + memberId
+    );
+  }
+
+  sendMessage(recipientId: string, content: string) {
+    return this.http.post<Message>(this.baseUrl + 'messages', {
+      recipientId,
+      content,
+    });
+  }
+
+  deleteMessage(id: string) {
+    return this.http.delete(this.baseUrl + 'messages/' + id);
+  }
+}
